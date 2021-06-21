2a4e9d7a565e15f951f8c5cf85b389eb46e1dfcd
Isak Cruz <isakcruzcs@gmail.com>
2021-06-21 19:47:21 -0300

    adicionando biblioteca

---
+++ biblioteca.h
@@ -0,0 +1,7 @@
+/*área de assinatura das funções do software*/
+
+//importação da implementação
+#include"implm.c"
+
+//assinatura que calcula o mmc entre três números informados pelo usuário
+int calculaMMC(int num1, int num2, int num3);
